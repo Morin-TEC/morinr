203c378e1c4eba5d9e9e4c09a3dd9082926a8ff9
Morin-TEC <70550103+Morin-TEC@users.noreply.github.com>
2022-05-30 11:15:15 -0500

    Sciptsql Update

--- Parcial 3/Scriptsql/19100222.sql
+++ Parcial 3/Scriptsql/19100222.sql
@@ -8,3 +8,23 @@ CREATE TABLE videojuego (
   inputPrecio varchar(30) NOT NULL,
   Fecha varchar(30) NOT NULL,
 );
+
+INSERT INTO videojuego VALUES (
+  2,'Minecraft','Sandbox',1000,'2 de Octubre del 2009' 
+);
+
+INSERT INTO videojuego VALUES (
+  4,'Call of Duty: Vanguard','Shooter',1400,'22 de Octubre del 2021' 
+);
+
+INSERT INTO videojuego VALUES (
+  9,'Spiderman','Accion',700,'4 de Abril del 2019' 
+);
+
+INSERT INTO videojuego VALUES (
+  6,'Mario 3D World','Plataformas',500,'22 de Noviembre del 2014' 
+);
+
+INSERT INTO videojuego VALUES (
+  8,'Splatoon 2','Shooter',1000,'30 de Mayo del 2019' 
+);
